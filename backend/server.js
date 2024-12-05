d27fdf0c7e76d377a5b27661f6bb332cbe0b901c
Sudhanya S <ssudhanya03@gmail.com>
2024-12-05 11:20:04 -0600

    Adding changes

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,6 @@ app.get("/api/getApiKey", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
